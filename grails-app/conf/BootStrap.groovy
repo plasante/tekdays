f61e801be0bb136382a800fb16ccbac72fcb9c35
Pierre Lasanat <pierre.lasante@videotron.ca>
2013-12-27 10:35:07 -0500

    5ieme commit

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,7 +3,19 @@ import tekdays.TekEvent
 class BootStrap {
 
     def init = { servletContext ->
-		def event1 = new TekEvent()
+		def event1 = new TekEvent(name: 'Gateway Code Camp',
+								  city: 'Saint Louis, MO',
+								  organizer: 'John Doe',
+								  venue: 'TBD',
+								  startDate: new Date('9/19/2014'),
+								  endDate: new Date('9/19/2014'),
+								  description: '''
+												  This conference will bring coders from
+												  across platforms, languages, and industries
+												  together for an exciting day of tips, tricks,
+												  and tech! Stay sharp! Stay at the top of your
+												  game! But, don't stay home!
+											   ''')
 		
 		if (!event1.save()) {
 			
