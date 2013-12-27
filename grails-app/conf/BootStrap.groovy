b120ca542d489941e775a5aad572b67d10c7eaa5
Pierre Lasanat <pierre.lasante@videotron.ca>
2013-12-27 10:27:55 -0500

    4ieme commit

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,21 @@
+import tekdays.TekEvent
+
 class BootStrap {
 
     def init = { servletContext ->
+		def event1 = new TekEvent()
+		
+		if (!event1.save()) {
+			
+		}
+		
+		def event2 = new TekEvent()
+		
+		if (!event2.save()) {
+			
+		}
     }
+	
     def destroy = {
     }
 }
